d6cc58dd212297b966670ef6f9349dfc65cc7663
nhuquynhvu <nhu_92@hotmail.com>
2025-02-11 16:09:32 +0100

    map ok

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import { createRoot } from "react-dom/client";
 import React from "react";
+import { createRoot } from "react-dom/client";
+import { Application } from "./modules/app/application";
 
-createRoot(document.getElementById("root")).render(<h1>Hello World 1</h1>);
+createRoot(document.getElementById("root")!).render(<Application />);
